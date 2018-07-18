8b26f25ad2d4ecf77fb8678374b0186994e02122
Justin Baum <justinba1010@gmail.com>
2018-07-18 13:44:21 -0400

    Main

--- 7-18/main.java
+++ 7-18/main.java
@@ -1,7 +1,11 @@
 import java.util.*;
 public class main {
-  public static void Main(String[] args) {
+  public static void main(String[] args) {
     AnimalCollector animals = new AnimalCollector();
-
-  }
-}
+    Scanner keyboard = new Scanner(System.in);
+    String message = "y";
+    while(message.equals("y")) {//This will run as long as the user wants it to
+      //This is where we will do all of our work
+    }//while
+  }//main
+}//main
